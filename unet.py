1e7e3226dff902f80feac06889391a2bba9d7885
Steven Harvey <harveysd@miamioh.edu>
2025-04-26 00:06:37 -0400

    Added and ran the beginning code created by ChatGPT for both the creation/training of the U-Net, as well as a basic evaluation of it's output.

--- unet.py
+++
@@ -1,7 +0,0 @@
-# Create and run a UNet to automatically create 1+px-wide skeletons of road
-# maps in a 256 x 256 rasterized images based off of thick, raw road data. This
-# data will be imported from OpenStreetMap.
-
-# Imports
-
-#Create UNet
